30ef4b09ebc8dddce4d66a0c382843d761cc5c66
Yuki Nagae <yuki.nagae1130@gmail.com>
2018-01-31 11:55:00 +0900

    Add setup.py to install dependencies (just example)

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='pbchain',
+    version='0.1.0',
+    description='Probabilistic Programming with Python and Chainer',
+    author='Yuki Nagae',
+    author_email='yuki.nagae1130@gmail.com',
+    url='https://github.com/yukinagae/pbchain',
+    license='MIT License',
+    install_requires=[
+        'numpy>=1.9.0',
+    ],
+)
