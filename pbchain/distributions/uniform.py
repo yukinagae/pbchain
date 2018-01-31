90790d8d3a7e8800dea311d343aa1156f0ea93ae
Yuki Nagae <yuki.nagae1130@gmail.com>
2018-02-01 02:22:58 +0900

    Add sample method for uniform dist (backend is scipy.stats)

--- pbchain/distributions/uniform.py
+++ pbchain/distributions/uniform.py
@@ -10,9 +10,14 @@ class Uniform():
     Uniform distribution
     """
 
-    def __init__(self, a, b):
+    def __init__(self, a, b, *args, **kwargs):
         """
         """
         self.a = a
         self.b = b
-        self.dist = uniform(a, b)
+
+    def sample(self):
+        return uniform.rvs(
+                loc=self.a,
+                scale=self.b - self.a
+                )
--- tests/test_distributions.py
+++ tests/test_distributions.py
@@ -4,3 +4,6 @@ def test_uniform_distribution():
     uniform = Uniform(1.0, 2.0)
     assert uniform.a == 1.0
     assert uniform.b == 2.0
+    for i in range(0, 10):
+        sample = uniform.sample()
+        assert 1.0 <= sample <= 2.0
