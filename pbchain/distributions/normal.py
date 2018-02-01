576985129bb85c6ae8efdd9a3a235b34a6427430
Yuki Nagae <yuki.nagae1130@gmail.com>
2018-02-01 18:01:19 +0900

    Add normal distribution

---
+++ pbchain/distributions/normal.py
@@ -0,0 +1,38 @@
+"""
+Normal distribution
+"""
+
+
+from chainer import Variable
+import chainer.functions as F
+import numpy as np
+
+
+class Normal():
+    """
+    Normal distribution
+    """
+
+    def __init__(self, mu, sigma):
+        """
+        initialize
+        """
+        self.mu = mu
+        self.sigma = sigma
+
+    def sample(self):
+        """sampling"""
+        eps = Variable(np.random.rand(1).astype(np.float32))
+        return self.mu + eps * self.sigma
+
+    def log_pdf(self, x):
+        """log probability distribution function"""
+        return -1 * (F.log(self.sigma) + 0.5 * np.log(2.0 * np.pi) + 0.5 * ((x - self.mu) / self.sigma) ** 2)
+
+    def analytic_mean(self):
+        """mean"""
+        return self.mu
+
+    def analytic_var(self):
+        """variance"""
+        return self.sigma ** 2
--- tests/test_distributions.py
+++ tests/test_distributions.py
@@ -8,6 +8,7 @@ from chainer import Variable
 import numpy as np
 
 from pbchain.distributions.uniform import Uniform
+from pbchain.distributions.normal import Normal
 
 
 def test_uniform_distribution():
@@ -15,11 +16,22 @@ def test_uniform_distribution():
     a = Variable(np.array([1.0]).astype(np.float32))
     b = Variable(np.array([3.0]).astype(np.float32))
     uniform = Uniform(a, b)
-    assert uniform.a.data[0] == a.data[0]
-    assert uniform.b.data[0] == b.data[0]
     x = Variable(np.array([1.95]).astype(np.float32))
     log_pdf = uniform.log_pdf(x)
     assert log_pdf.data[0] == pytest.approx(-0.6931471824645996, abs=1e-5)
     for _ in range(0, 100):
         sample = uniform.sample()
         assert a.data[0] <= sample.data[0] <= b.data[0]
+
+
+def test_normal_distribution():
+    """normal distribution test"""
+    a = Variable(np.array([1.0]).astype(np.float32))
+    b = Variable(np.array([3.0]).astype(np.float32))
+    normal = Normal(a, b)
+    x = Variable(np.array([1.95]).astype(np.float32))
+    log_pdf = normal.log_pdf(x)
+    assert log_pdf.data[0] == pytest.approx(-2.067689895629883, abs=1e-5)
+    # for _ in range(0, 100):
+    #     sample = normal.sample()
+    #     TODO: how to assert?
