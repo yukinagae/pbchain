086bf1a2aa979dc91de2f1842f766605416a2581
Yuki Nagae <yuki.nagae1130@gmail.com>
2018-02-15 22:23:51 +0900

    Remove redundant doc

--- pbchain/distributions/uniform.py
+++ pbchain/distributions/uniform.py
@@ -26,20 +26,16 @@ class Uniform(RandomVariable, Variable):
         super(Uniform, self).__init__(*args, **kwargs)
 
     def sample(self, *args, **kwargs):
-        """sampling"""
         eps = np.random.random_sample()
         return self.a + eps * (self.b - self.a)
 
     def log_pdf(self, x, *args, **kwargs):
-        """log probability distribution function"""
         if x.data[0] < self.a.data[0] or x.data[0] > self.b.data[0]:
             return F.log(0.0)
         return F.log(1.0 / (self.b - self.a))
 
     def mean(self, *args, **kwargs):
-        """mean"""
         return 0.5 * (self.a + self.b)
 
     def var(self, *args, **kwargs):
-        """variance"""
         return (self.b - self.a) ** 2 / 12
